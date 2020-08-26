d1f5e266feefd0003016c166ad4a0ec40be2c2df
kghugo <howtostudycantonese@gmail.com>
2020-08-26 21:24:44 +0800

    :construction: Set state with useState() and useEffect()

--- static/js/components/appListing.js
+++ static/js/components/appListing.js
@@ -1,12 +1,16 @@
 import { h, render, Component } from 'preact'
 import { useState, useEffect } from 'preact/hooks'
+import { R } from 'ramda'
 
 const AppListing = (props) => {
+  const [app, setApp] = useState([])
+
   useEffect(
     async () => {
-      const apiUrl = 'https://rss.itunes.apple.com/api/v1/hk/ios-apps/top-free/all/10/explicit.json'
+      const apiUrl = 'https://cors-anywhere.herokuapp.com/' + 'https://rss.itunes.apple.com/api/v1/hk/ios-apps/top-free/all/10/explicit.json'
       return await fetch(apiUrl)
         .then(response => response.json())
+        .then(data => setApp(data))
     }
   )
 
