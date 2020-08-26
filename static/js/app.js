c4da957d84c13d0de617679a3f9db201f3e797ec
kghugo <howtostudycantonese@gmail.com>
2020-08-26 20:54:36 +0800

    :construction: Create AppRecommendation component

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,13 @@
 import { h, render, Component } from 'preact'
 import { useState, useEffect } from 'preact/hooks'
 import AppListing from './components/appListing.js'
+import AppRecommendation from './components/appRecommendation.js'
 
 const container = document.getElementById('primary')
 
 const App = (props) => {
   return (
-    <p>Hello world</p>,
+    <AppRecommendation />,
     <AppListing />
   )
 }
---
+++ static/js/components/appRecommendation.js
@@ -0,0 +1,10 @@
+import { h, render, Component } from 'preact'
+import { useState, useEffect } from 'preact/hooks'
+
+const AppRecommendation = (props) => {
+  return (
+    <p>This is app recommendation</p>
+  )
+}
+
+export default AppRecommendation
