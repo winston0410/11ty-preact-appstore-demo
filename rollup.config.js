4df48391a89b5893e7f77e3349d80cbfb99c816b
kghugo <howtostudycantonese@gmail.com>
2020-08-26 23:21:50 +0800

    :wrench: Add PostCSS plugin in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import multiInput from 'rollup-plugin-multi-input'
+import postcss from 'rollup-plugin-postcss'
 const {
   terser
 } = require('rollup-plugin-terser')
@@ -28,6 +29,29 @@ export default [{
     ]
   }],
   plugins: [
+    postcss({
+      plugins: [
+        require('postcss-preset-env')({
+          stage: 3
+        }),
+        require('postcss-import')({}),
+        require('postcss-sort-media-queries')({
+          sort: 'mobile-first'
+        }),
+        require('postcss-momentum-scrolling')([
+          'scroll'
+        ]),
+        require('postcss-nested')({}),
+        require('autoprefixer')({}),
+        require('cssnano')({
+          preset: ['advanced', {
+            discardComments: {
+              removeAll: true
+            }
+          }]
+        })
+      ]
+    }),
     babel({
       babelHelpers: 'runtime',
       skipPreflightCheck: true
