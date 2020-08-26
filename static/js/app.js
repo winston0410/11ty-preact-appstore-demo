57475c7410f1c00cb3e5e752b962872d5672bac5
kghugo <howtostudycantonese@gmail.com>
2020-08-26 20:49:45 +0800

    :heavy_plus_sign: Add @rollup/plugin-babel as dependency

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,14 @@
 import { h, render, Component } from 'preact'
 import { useState, useEffect } from 'preact/hooks'
-import AppListing from './components/AppListing.js'
+import AppListing from './components/appListing.js'
 
-const App = () => {
+const container = document.getElementById('primary')
 
+const App = (props) => {
+  return (
+    <p>Hello world</p>,
+    <AppListing />
+  )
 }
+
+render(<App />, container)
