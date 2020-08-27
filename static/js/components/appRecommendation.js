eb2f381064abe9f4d5a32d3c6b39b6f7ec5bfff7
Hugo <hugosum@PC3343.local>
2020-08-27 17:21:26 +0800

    :construction: Import other library to appRecommendation

--- static/js/components/appRecommendation.js
+++ static/js/components/appRecommendation.js
@@ -1,6 +1,11 @@
 import { h, render, Component } from 'preact'
 import { useState, useEffect } from 'preact/hooks'
 import './appListing.css'
+import * as R from 'ramda'
+import {
+  fetchRequest
+} from '../utilities/helper.js'
+import { openDB, deleteDB, wrap, unwrap } from 'idb'
 
 const AppRecommendation = (props) => {
   return (
