2ce89137830ac02c5bf0f3a0c085cd360f45298d
kghugo <howtostudycantonese@gmail.com>
2020-08-27 21:04:04 +0800

    :construction: Fetch data for appRecommendation.js

--- static/js/components/appRecommendation.js
+++ static/js/components/appRecommendation.js
@@ -9,6 +9,32 @@ import {
 import { openDB, deleteDB, wrap, unwrap } from 'idb'
 
 const AppRecommendation = (props) => {
+  const [app, setApp] = useState([])
+
+  const accessResults = R.pipe(
+    R.prop('feed'),
+    R.prop('results')
+  )
+
+  useEffect(
+    async () => {
+      const appDB = await openDB('app', 1, {
+        upgrade (db) {
+          db.createObjectStore('app-recommendation')
+        }
+      })
+
+      const apiUrl = 'https://cors-anywhere.herokuapp.com/https://rss.itunes.apple.com/api/v1/hk/ios-apps/top-grossing/all/10/explicit.json'
+
+      const response = await fetchRequest(apiUrl)
+
+      console.log(response)
+
+      setApp(response)
+    },
+    []
+  )
+
   return (
     <p>This is app recommendation</p>
   )
